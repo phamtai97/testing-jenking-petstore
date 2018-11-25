d2fdc62fd6868337e3851c4626ab5dd50071a4ed
Tai. Pham Tinh Hong Tan <taiptht@vng.com.vn>
2018-11-25 12:38:54 +0700

    update

--- model/pet.js
+++ model/pet.js
@@ -7,7 +7,7 @@ let ListData = [
     {id: 6, name: 'Kitty06', status: 'available'},
     {id: 7, name: 'Kitty07', status: 'available'},
     {id: 8, name: 'Kitty008', status: 'available'},
-    {id: 9, name: 'Kitty0009', status: 'available'},
+    {id: 9, name: 'Kitty00009', status: 'available'},
 ];
 module.exports.find = (callback) => {
     callback(null, ListData);
